a3975702323a6bdfe0e017346743d34cd9a527c1
Ben Andrews <justbenuk@gmail.com>
2026-09-05 12:34:10 +0100

    Placeholder Text
    I have added the placeholder text for the home page. This gives an idea
    of whats coming to anyone that may find the domain

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>Working Project</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen p-6">
+      <h1 className="text-3xl lg:text-7xl font-semibold">Work in Progress</h1>
+      <p className="md:w-1/2 lg:w-1/3 text-center">
+        I am currently working on a platform for Everything Tamworth for the
+        community of Tamworth. From jobs to events, news and local business.
+        What ever you need will be here
+      </p>
+    </div>
+  );
 }
