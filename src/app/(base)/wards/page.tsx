da01506f345606c7ded18748e073a87631d7286b
Ben Andrews <justbenuk@gmail.com>
2026-09-17 22:22:06 +0100

    made change to header (#13)

--- src/app/(base)/wards/page.tsx
+++ src/app/(base)/wards/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
     "Explore Tamworth wards and find local news, crime information, jobs and councillors.",
 };
 
+const dynamic = "force-dynamic";
+
 export default async function WardsPage() {
   const wards = await FetchAllWardsAction();
   return <WardListMapContainer wards={wards} />;
