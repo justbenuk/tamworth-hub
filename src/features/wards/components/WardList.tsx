3ddb2ac31867dbd23de0985494fe27f5a4a83c73
Ben Andrews <justbenuk@gmail.com>
2026-09-17 22:47:22 +0100

    build fix

--- src/features/wards/components/WardList.tsx
+++ src/features/wards/components/WardList.tsx
@@ -3,5 +3,6 @@ import WardListMapContainer from "./maps/WardListMapContainer";
 
 export default async function WardList() {
   const wards = await FetchAllWardsAction();
+  if (!wards) return <div>No Wards</div>;
   return <WardListMapContainer wards={wards} />;
 }
