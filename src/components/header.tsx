10a419b616c170335d0c37da87e3c0ba607c8a05
Ben Andrews <justbenuk@gmail.com>
2026-09-17 22:33:19 +0100

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
         <div className="justify-self-center">
           <MenuList />
         </div>
-        <div className="justify-self-end">_</div>
+        <div className="justify-self-end">A</div>
       </header>
     </PageContainer>
   );
