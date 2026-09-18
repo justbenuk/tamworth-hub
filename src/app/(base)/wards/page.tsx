18b17e81373ecda87ab46988f39e7d63ce295051
Ben Andrews <justbenuk@gmail.com>
2026-09-18 13:03:56 +0100

    looking at suspense,
    testing the build if i use suspense

--- src/app/(base)/wards/page.tsx
+++ src/app/(base)/wards/page.tsx
@@ -1,5 +1,8 @@
-import WardList from "@/features/wards/components/WardList";
+import WardListMapContainer from "@/features/wards/components/maps/WardListMapContainer";
+import { FetchAllWardsAction } from "@/features/wards/WardActions";
+
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Wards",
@@ -7,6 +10,15 @@ export const metadata: Metadata = {
     "Explore Tamworth wards and find local news, crime information, jobs and councillors.",
 };
 
-export default function WardsPage() {
-  return <WardList />;
+export default async function WardsPage() {
+  const wards = await FetchAllWardsAction();
+  if (!wards) return <div>No Wards</div>;
+
+  return (
+    <>
+      <Suspense fallback={<p>loading</p>}>
+        <WardListMapContainer wards={wards} />;
+      </Suspense>
+    </>
+  );
 }
--- src/features/wards/components/WardList.tsx
+++
@@ -1,8 +0,0 @@
-import { FetchAllWardsAction } from "@/features/wards/WardActions";
-import WardListMapContainer from "./maps/WardListMapContainer";
-
-export default async function WardList() {
-  const wards = await FetchAllWardsAction();
-  if (!wards) return <div>No Wards</div>;
-  return <WardListMapContainer wards={wards} />;
-}
